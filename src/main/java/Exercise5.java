93e0a86f09029af589fb54e81b5f77a9044f049a
umranrazack02 <umranrazack02@outlook.com>
2021-08-01 19:54:23 -0400

    challenges

--- src/main/java/Exercise5.java
+++ src/main/java/Exercise5.java
@@ -10,15 +10,52 @@ public class Exercise5 {
         System.out.print("What is the second number? ");
         String n2 = in.nextLine();
 
-        int first = Integer.parseInt(n1);
-        int second = Integer.parseInt(n2);
+        double first = 0;
+        double second =  0;
 
-        int add = first + second;
-        int sub = first - second;
-        int mult = first * second;
-        int div = first / second;
+        // challenge dont let user continue without entering numerica value
+        try {
+            first = Double.parseDouble(n1);
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter a numeric value!");
+            System.exit(0);
+        }
 
-        System.out.printf("%d + %d = %d\n%d - %d = %d\n%d * %d = %d\n%d / %d = %d\n", first, second, add, first, second, sub, first, second, mult, first, second, div);
+        try {
+            second = Double.parseDouble(n2);
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter a numeric value!");
+            System.exit(0);
+        }
 
+        // challenge no negative numbers
+        if (first < 0 || second < 0) {
+            System.out.println("Please enter a positive numeric value!");
+            System.exit(0);
+        }
+
+        double add = addition(first, second);
+        double sub = subtraction(first, second);
+        double mult = multiplication(first, second);
+        double div = division(first, second);
+
+        System.out.printf("%.2f + %.2f = %.2f\n%.2f - %.2f = %.2f\n%.2f * %.2f = %.2f\n%.2f / %.2f = %.2f\n", first, second, add, first, second, sub, first, second, mult, first, second, div);
+    }
+
+    // challenge break up into functions
+    public static double addition(double first, double second) {
+        return first + second;
+    }
+
+    public static double subtraction(double first, double second) {
+        return first - second;
+    }
+
+    public static double multiplication(double first, double second) {
+        return first * second;
+    }
+
+    public static double division(double first, double second) {
+        return first / second;
     }
 }
